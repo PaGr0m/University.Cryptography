4e0283f18ceb7b19e83d8258b3390b7e5ce043b2
Pavel Gromov <pagrom27@gmail.com>
2018-09-18 00:35:04 +1000

    LIPO | Complete Lexical Analyzer

--- linguisticProgramming/MainConsole/main.cpp
+++ linguisticProgramming/MainConsole/main.cpp
@@ -6,7 +6,7 @@ const QString PATH_TO_FILE = ":/input/resourses/programTest.txt";
 
 const QStringList serviceWords = (QStringList() << "Var");
 const QStringList unaryOperations = (QStringList() << "+" << "-" << "*" << "/");
-const QStringList signs = (QStringList() << ";" << "." << "=");
+const QStringList signs = (QStringList() << ";" << "." << "=" << ":");
 const QStringList otherSymbols = (QStringList() << ":=" << ",");
 
 const QRegExp regLetters("[A-Za-z]");
@@ -27,30 +27,18 @@ QString readFromFile(QString pathToFile)
 
 bool isIdent(QString word)
 {
-    int count = 0;
-
     foreach (QChar symbol, word)
-        if (symbol.isLetter())
-            count++;
-
-    if (count == word.length())
-        return true;
-    else
-        return false;
+        if (!symbol.isLetter())
+            return false;
+    return true;
 }
 
 bool isConst(QString word)
 {
-    int count = 0;
-
     foreach (QChar symbol, word)
-        if (symbol.isDigit())
-            count++;
-
-    if (count == word.length())
-        return true;
-    else
-        return false;
+        if (!symbol.isDigit())
+            return false;
+    return true;
 }
 
 QStringList readOnList(QString pathToFile)
@@ -69,57 +57,121 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QStringList str = readOnList(PATH_TO_FILE);
-
-//    QStringList str;
-//    QString qwe = readFromFile(PATH_TO_FILE);
-//    foreach (QString line, qwe.split("\n"))
-//    {
-//        str.append(line);
-//    }
-
-
-//    foreach( const QString& str, strings )
-    foreach (QString line, str)
+    QFile file(PATH_TO_FILE);
+    if (file.open(QIODevice::ReadOnly))
     {
-        foreach (QString word, line.split(" "))
+        while (!file.atEnd())
         {
-            qDebug() << word;
+            QByteArray tmp;
+            QByteArray symbol = file.read(1);
 
-            if (serviceWords.contains(word))
+            // IDENT
+            if (isIdent(symbol))
             {
-                qDebug() << word + " -> serviceWords";
+                while (true)
+                {
+                    tmp = file.read(1);
+                    if(isIdent(tmp))
+                        symbol.append(tmp);
+                    else
+                        break;
+                }
+
+                if (serviceWords.contains(symbol))
+                    qDebug() << symbol + " --> serviceWords";
+                else
+                    qDebug() << symbol + " --> IDENT";
+
+                if (unaryOperations.contains(tmp))
+                    qDebug() << tmp + " --> UNARY";
+                else if (signs.contains(tmp))
+                    qDebug() << tmp + " --> SIGNS";
+                else if (otherSymbols.contains(tmp))
+                    qDebug() << tmp + " --> OTHER";
+
             }
-            else if (unaryOperations.contains(word))
+
+            // CONST
+            else if (isConst(symbol))
             {
-                qDebug() << word + " -> unaryOperations";
+                while (true)
+                {
+                    tmp = file.read(1);
+                    if (isConst(tmp))
+                        symbol.append(tmp);
+                    else
+                        break;
+                }
+                qDebug() << symbol + " --> CONST";
+
+                if (unaryOperations.contains(tmp))
+                    qDebug() << tmp + " --> UNARY";
+                else if (signs.contains(tmp))
+                    qDebug() << tmp + " --> SIGNS";
+                else if (otherSymbols.contains(tmp))
+                    qDebug() << tmp + " --> OTHER";
             }
-            else if (signs.contains(word))
-            {
-                qDebug() << word + " -> signs";
-            }
-            else if (isConst(word))
-            {
-                qDebug() << word + " -> const";
-            }
-            else if (isIdent(word))
-            {
-                qDebug() << word + " -> ident";
-            }
-            else if (otherSymbols.contains(word))
-            {
-                qDebug() << word + " -> other";
-            }
-            else
-            {
-                qDebug() << "!!! MAIN ERROR !!!";
-                return 1;
-            }
-//            qDebug() << "";
+
+            // UNART OPERATIONS
+            else if (unaryOperations.contains(symbol))
+                qDebug() << symbol + " --> UNARY";
+
+            // SIGNS
+            else if (signs.contains(symbol))
+                qDebug() << symbol + " --> SIGNS";
+
+            // OTHER
+            else if (otherSymbols.contains(symbol))
+                qDebug() << symbol + " --> OTHER";
         }
-
-//        qDebug() << line;
     }
 
     return a.exec();
+}
+
+void test()
+{
+//        foreach (QString line, str)
+//        {
+//            foreach (QString word, line.split(" "))
+//            {
+//                qDebug() << word;
+
+//                if (serviceWords.contains(word))
+//                {
+//                    qDebug() << word + " -> serviceWords";
+//                }
+//                else if (word.isEmpty())
+//                {
+
+//                }
+//                else if (unaryOperations.contains(word))
+//                {
+//                    qDebug() << word + " -> unaryOperations";
+//                }
+//                else if (signs.contains(word))
+//                {
+//                    qDebug() << word + " -> signs";
+//                }
+//                else if (isConst(word))
+//                {
+//                    qDebug() << word + " -> const";
+//                }
+//                else if (isIdent(word))
+//                {
+//                    qDebug() << word + " -> ident";
+//                }
+//                else if (otherSymbols.contains(word))
+//                {
+//                    qDebug() << word + " -> other";
+//                }
+//                else
+//                {
+//                    qDebug() << "!!! MAIN ERROR !!!";
+////                    return 1;
+//                }
+////                qDebug() << "";
+//            }
+////            qDebug() << line;
+//        }
 }
