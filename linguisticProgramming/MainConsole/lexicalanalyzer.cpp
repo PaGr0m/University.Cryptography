dc38b692c37267b764645714a4bf33da7bcb867f
Pavel Gromov <pagrom27@gmail.com>
2018-09-23 05:16:58 +1000

    LIPO | Rework algorithm
    TODO: add error exception, A-Za-z alphabet, check ident

--- linguisticProgramming/MainConsole/lexicalanalyzer.cpp
+++ linguisticProgramming/MainConsole/lexicalanalyzer.cpp
@@ -4,3 +4,145 @@ LexicalAnalyzer::LexicalAnalyzer()
 {
 
 }
+
+LexicalAnalyzer::LexicalAnalyzer(QString path)
+{
+    file.setFileName(path);
+    readFileToString();
+}
+
+bool LexicalAnalyzer::isConst(QString word)
+{
+    foreach (QChar symbol, word)
+        if (!symbol.isDigit())
+            return false;
+    return true;
+}
+
+bool LexicalAnalyzer::isIdent(QString word)
+{
+    foreach (QChar symbol, word)
+        if (!symbol.isLetter())
+            return false;
+    return true;
+}
+
+bool LexicalAnalyzer::isUnary(QString word)
+{
+    return unaryOperations.contains(word);
+}
+
+bool LexicalAnalyzer::isSigns(QString word)
+{
+    return signs.contains(word);
+}
+
+bool LexicalAnalyzer::isOther(QString word)
+{
+    return otherSymbols.contains(word);
+}
+
+void LexicalAnalyzer::readFileToString()
+{
+    QString string;
+
+    if (file.open(QIODevice::ReadOnly))
+        string.append(file.readAll());
+
+    this->text = string;
+}
+
+void LexicalAnalyzer::setFile(QString path)
+{
+    file.setFileName(path);
+}
+
+void LexicalAnalyzer::setPath(QString path)
+{
+    this->path = path;
+}
+
+void LexicalAnalyzer::printInfo(QString value, QString type)
+{
+    if (printFlag)
+        qDebug() << value + " --> " + type;
+}
+
+void LexicalAnalyzer::run()
+{
+    setFile(this->path);
+    readFileToString();
+
+    while (index < text.length())
+    {
+        QString word;
+        QString symbol = text.at(index);
+
+        // IDENT
+        if (isIdent(symbol))
+        {
+            while (true)
+            {
+                if (isIdent(text.at(index)))
+                {
+                    word.append(text.at(index));
+                    index++;
+                }
+                else
+                {
+                    index--;
+                    break;
+                }
+            }
+
+            if (serviceWords.contains(word))
+                printInfo(word, "SERVICE_WORDS");
+            else
+                printInfo(word, "IDENT");
+        }
+
+        // CONST
+        else if (isConst(symbol))
+        {
+            while (true)
+            {
+                if (isConst(text.at(index)))
+                {
+                    word.append(text.at(index));
+                    index++;
+                }
+                else
+                {
+                    index--;
+                    break;
+                }
+            }
+            printInfo(word, "CONST");
+        }
+
+        // UNART OPERATIONS
+        else if (unaryOperations.contains(symbol))
+            printInfo(symbol, "UNARY");
+        // SIGNS
+        else if (signs.contains(symbol))
+            printInfo(symbol, "SIGNS");
+
+        // OTHER
+        else if (otherSymbols.contains(symbol))
+            printInfo(symbol, "OTHER");
+
+        // ERROR
+//        else
+//        {
+//            // TODO: create error function, check "\n", "\r", " "
+//            // Alphabet is A-Za-z
+//            qDebug() << "Error";
+//            break;
+//        }
+
+        // NEXT ITERATOR
+        index++;
+    }
+}
+
+
--- linguisticProgramming/MainConsole/lexicalanalyzer.h
+++ linguisticProgramming/MainConsole/lexicalanalyzer.h
@@ -1,11 +1,46 @@
 #ifndef LEXICALANALYZER_H
 #define LEXICALANALYZER_H
 
+#include <QFile>
+#include <QString>
+#include <QVector>
+#include <QDebug>
 
 class LexicalAnalyzer
 {
 public:
     LexicalAnalyzer();
+    LexicalAnalyzer(QString path);
+
+    bool isConst(QString word);
+    bool isIdent(QString word);
+    bool isOther(QString word);
+    bool isSigns(QString word);
+    bool isUnary(QString word);
+
+    void readFileToString();
+
+    void setFile(QString path);
+    void setPath(QString path);
+
+    void printInfo(QString value, QString type);
+    void run();
+
+private:
+    const QVector<QString> serviceWords = {"Var"};
+    const QVector<QString> unaryOperations = {"+", "-", "*", "/"};
+    const QVector<QString> signs = {";", ".", "=", ":"};
+    const QVector<QString> otherSymbols = {":=", ".", " "};
+
+private:
+    bool printFlag = true;
+    int index = 0;
+
+    QFile file;
+    QString text;
+    QString path = "";
+
+    QVector<QString> lexemBox;
 };
 
 #endif // LEXICALANALYZER_H
